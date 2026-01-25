cffd6d4f96efd5b4682faae2056dd46723f5e5c5
Mav <riherr00@gmail.com>
2026-01-25 11:23:14 +0300

    update patient endpoint

--- patient-service/src/main/java/com/mav/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/mav/patientservice/controller/PatientController.java
@@ -8,24 +8,31 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/patients")
 class PatientController {
-
+    
     private final PatientService patientService;
-
+    
     PatientController(PatientService patientService) {
         this.patientService = patientService;
     }
-
+    
     @GetMapping
     public ResponseEntity<List<PatientResponseDTO>> getPatients() {
         return ResponseEntity.ok(patientService.getPatients());
     }
-
+    
     @PostMapping
     public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
         return ResponseEntity.ok(patientService.createPatient(patientRequestDTO));
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id,
+                                                            @Valid @RequestBody PatientRequestDTO patientRequestDTO) {
+        return ResponseEntity.ok(patientService.updatePatient(id, patientRequestDTO));
     }
 }
