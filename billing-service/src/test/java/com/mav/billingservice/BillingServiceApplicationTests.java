15b37aeed04940309eb9f561e67135ca01f5c595
Mav <riherr00@gmail.com>
2026-01-27 08:02:40 +0300

    Billing service init

---
+++ billing-service/src/test/java/com/mav/billingservice/BillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mav.billingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServiceApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
