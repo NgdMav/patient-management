8d97ff7210b4dfda3e4f631066d5188acccd5442
Mav <riherr00@gmail.com>
2026-03-27 16:18:10 +0300

    Now all it works on kafka:9092. Analytics service recive message

--- analytics-service/src/test/java/com/mav/analyticsservice/AnalyticsServiceApplicationTests.java
+++ analytics-service/src/test/java/com/mav/analyticsservice/AnalyticsServiceApplicationTests.java
@@ -3,11 +3,14 @@ package com.mav.analyticsservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        // Don't require a running Kafka broker during tests/build-image
+        "spring.kafka.admin.enabled=false",
+        "spring.kafka.listener.auto-startup=false"
+})
 class AnalyticsServiceApplicationTests {
-    
+
     @Test
     void contextLoads() {
     }
-    
 }
--- patient-service/src/test/java/com/mav/patientservice/PatientServiceApplicationTests.java
+++ patient-service/src/test/java/com/mav/patientservice/PatientServiceApplicationTests.java
@@ -2,8 +2,13 @@ package com.mav.patientservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+    import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@SpringBootTest(properties = {
+        // Don't require a running Kafka broker during tests/build-image
+        "spring.kafka.admin.enabled=false"
+})
 class PatientServiceApplicationTests {
 
     @Test
