b054e9558452c90376b8f99fc597c715c2fb8ffb
Mav <riherr00@gmail.com>
2026-04-01 20:19:23 +0300

    AuthService

---
+++ auth-service/src/main/java/com/mav/authservice/service/AuthService.java
@@ -0,0 +1,27 @@
+package com.mav.authservice.service;
+
+import com.mav.authservice.dto.LoginRequestDTO;
+import com.mav.authservice.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+    
+    private final UserService userService;
+    
+    public AuthService(UserService userService) {
+        this.userService = userService;
+    }
+    
+    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
+        Optional<String> token = userService
+                .findByEmail(loginRequestDTO.getEmail())
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(),
+                        u.getPassword()))
+                .map(u -> jwtUtil.generateToket(u.getEmail(), u.getRole()));
+        
+        return token;
+    }
+}
