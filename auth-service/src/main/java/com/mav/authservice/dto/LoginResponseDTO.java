ba313c57e2298f1186ef687b33bb57a82f766298
Mav <riherr00@gmail.com>
2026-04-01 19:43:31 +0300

    LoginResponseDTO

---
+++ auth-service/src/main/java/com/mav/authservice/dto/LoginResponseDTO.java
@@ -0,0 +1,13 @@
+package com.mav.authservice.dto;
+
+public class LoginResponseDTO {
+    private final String token;
+    
+    public LoginResponseDTO(String token) {
+        this.token = token;
+    }
+    
+    public String getToken() {
+        return token;
+    }
+}
