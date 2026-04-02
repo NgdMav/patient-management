bdcaa8f187f760601c0fc84e1da607ab2467e66a
Mav <riherr00@gmail.com>
2026-04-02 12:00:44 +0300

    JwtUtil

--- auth-service/src/main/java/com/mav/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/mav/authservice/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.mav.authservice.service;
 
 import com.mav.authservice.dto.LoginRequestDTO;
+import com.mav.authservice.util.JwtUtil;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +12,12 @@ public class AuthService {
     
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil  jwtUtil;
     
-    public AuthService(UserService userService, PasswordEncoder passwordEncoder) {
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
     
     public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
@@ -23,6 +26,6 @@ public class AuthService {
                 .findByEmail(loginRequestDTO.getEmail())
                 .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(),
                         u.getPassword()))
-                .map(u -> jwtUtil.generateToket(u.getEmail(), u.getRole()));
+                .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
     }
 }
